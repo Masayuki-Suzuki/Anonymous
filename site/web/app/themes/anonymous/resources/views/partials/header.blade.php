f372239cc57c50fc1e1ed84bbc5e82f9869156db
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2017-08-22 20:13:35 -0700

    First Commit

---
+++ site/web/app/themes/anonymous/resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header class="header">
+  <div class="header__wrap">
+    <a href="{{ home_url('/') }}" class="logo">
+      <img class="logo__symbol logo__animate" src="{{ get_stylesheet_directory_uri() }}/dist/images/logo.svg" alt="">
+      <h1 class="logo__brand logo__animate">anonymous<span class="subtitle">front-end developer in Vancouver</span></h1>
+    </a>
+    <nav class="gNav">
+      <ul class="gNav__body">
+        <li class="gNav__list"><a class="topPage" href="{{ home_url('/') }}"></a></li>
+        <li class="gNav__list"><a class="github" href="https://github.com/Masayuki-Suzuki" target="_blank"></a></li>
+        <li class="gNav__list"><a class="twitter" href="https://twitter.com/9_Planet" target="_blank"></a></li>
+        <li class="gNav__list"><a class="facebook" href="https://www.facebook.com/masayuki.suzuki.50" target="_blank"></a></li>
+        <li class="gNav__list"><a class="linkedin" href="https://www.linkedin.com/in/masayuki-suzuki/" target="_blank"></a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
