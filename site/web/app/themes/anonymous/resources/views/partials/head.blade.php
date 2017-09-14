0fbaee963b169743b91fcd7e08d7d98f688f0055
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2017-09-14 08:20:03 -0700

    [Fix] head.php

--- site/web/app/themes/anonymous/resources/views/partials/head.blade.php
+++ site/web/app/themes/anonymous/resources/views/partials/head.blade.php
@@ -3,13 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
-  <!-- Global Site Tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106469679-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)};
-    gtag('js', new Date());
-    gtag('config', 'UA-106469679-1');
-  </script>
   @php(wp_head())
 </head>
