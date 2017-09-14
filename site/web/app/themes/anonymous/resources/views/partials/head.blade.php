460041094339129f6d7a87dfd0794cf3db563c05
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2017-09-13 20:07:23 -0700

    [Fix] Remove Disqus Developer code and change image file name

--- site/web/app/themes/anonymous/resources/views/partials/head.blade.php
+++ site/web/app/themes/anonymous/resources/views/partials/head.blade.php
@@ -3,8 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
-  <script type="text/javascript">
-    var disqus_developer = 1; // Mode on developer
-  </script>
   @php(wp_head())
 </head>
