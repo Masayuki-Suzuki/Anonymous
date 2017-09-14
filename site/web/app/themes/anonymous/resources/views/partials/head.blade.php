37b7b0eb9b3058d8be0e4c9a07f52fefdc21a77b
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2017-09-14 08:03:11 -0700

    [Add] Add GA Tag and CSS for Privacy policy.

--- site/web/app/themes/anonymous/resources/views/partials/head.blade.php
+++ site/web/app/themes/anonymous/resources/views/partials/head.blade.php
@@ -2,6 +2,15 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-  <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
   @php(wp_head())
+  <!-- Global Site Tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106469679-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};
+    gtag('js', new Date());
+    gtag('config', 'UA-106469679-1');
+  </script>
+
 </head>
