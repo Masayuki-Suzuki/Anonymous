fcfc30002d1a69d3f76197df91f4e19f2865e6fa
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2017-09-15 21:33:53 -0700

    [Fix] icon file path.

--- site/web/app/themes/anonymous/resources/views/partials/head.blade.php
+++ site/web/app/themes/anonymous/resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="{{ get_stylesheet_directory_uri() }}/dist/img/favicon_c5f1ad3b.ico">
+  <link rel="icon" type="image/x-icon" href="{{ get_stylesheet_directory_uri() }}/dist/imgages/favicon_c5f1ad3b.ico">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
   @php(wp_head())
 </head>
