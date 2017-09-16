d6ce709dae55df1406751615a9fc405f45a1dede
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2017-09-15 21:37:44 -0700

    [Modify] allow from any domain for development.

--- site/web/app/themes/anonymous/app/filters.php
+++ site/web/app/themes/anonymous/app/filters.php
@@ -82,7 +82,7 @@ add_filter( 'wp_list_categories', function($output){
 add_filter( 'wp_headers', function($headers){
   global $wp;
   if (preg_match (‘/wp-json/’,$wp->request)){
-    $headers['Access-Control-Allow-Origin'] = 'https://masa.works/';
+    $headers['Access-Control-Allow-Origin'] = '*';
     $headers['Access-Control-Allow-Credentials'] = 'true';
     return $headers;
   }
